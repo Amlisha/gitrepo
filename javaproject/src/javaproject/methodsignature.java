ab13aa0498c18f70ac56520ce340a6571d205bf1
Amlisha <2302182@dhempecollege.edu.in>
2025-11-07 05:43:34 +0530

    this is my first initial commit

--- javaproject/src/javaproject/methodsignature.java
+++ javaproject/src/javaproject/methodsignature.java
@@ -3,13 +3,14 @@ package javaproject;
 public class methodsignature {
 
 	public int add(int a, int b, int c) {
-		return a+b;
+		return a+b+c;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		methodsignature calc=new methodsignature();
-		System.out.println(calc.add(1,2, 5));
+		int res=calc.add(1, 2, 3);
+		System.out.println("The sum is "+res);
 	}
 
 }
